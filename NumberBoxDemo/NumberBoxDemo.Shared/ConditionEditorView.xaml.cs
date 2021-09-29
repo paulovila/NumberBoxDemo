0b9095aef382a1c3d8633a0cd978471f6392dd5d
Paulo Vila <paulovila@live.com>
2021-09-29 21:15:53 +0100

    Add redundant calss inheritance

--- NumberBoxDemo/NumberBoxDemo.Shared/ConditionEditorView.xaml.cs
+++ NumberBoxDemo/NumberBoxDemo.Shared/ConditionEditorView.xaml.cs
@@ -1,8 +1,9 @@
 ﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 
 namespace NumberBoxDemo
 {   
-    public sealed partial class ConditionEditorView 
+    public sealed partial class ConditionEditorView : UserControl
     {
         public static readonly DependencyProperty ViewModelProperty =
                   DependencyProperty.Register(nameof(ViewModel), typeof(ConditionEditor), typeof(ConditionEditorView), new PropertyMetadata(null));
